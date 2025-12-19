7c84a7824757318b799f0f883382ddeb7430506f
RamazanMakhmatov <saadmakhmatov@gmail.com>
2025-12-18 23:33:20 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const formWrapper = document.getElementById("formWrapper");
 contactBtn.addEventListener("click", function () {
   formWrapper.classList.toggle("hidden");
 });
+const form = document.getElementById("contactForm");
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (nameInput.value === "" || phoneInput.value === "") {
+    message.textContent = "❌ Заполните все поля";
+    message.style.color = "red";
+  } else {
+    message.textContent = "✅ Заявка отправлена!";
+    message.style.color = "green";
+
+    nameInput.value = "";
+    phoneInput.value = "";
+  }
+});
