87e6ee3632793bb7de3fc510cc9763dc63774291
RamazanMakhmatov <saadmakhmatov@gmail.com>
2025-12-18 22:33:01 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const ctaBtn = document.getElementById('ctaBtn');
+const contactBtn = document.getElementById('contactBtn');
+
+ctaBtn.addEventListener('click', () => {
+  alert('Спасибо за интерес к курсу!');
+});
+
+contactBtn.addEventListener('click', () => {
+  alert('Заявка отправлена!');
+});
